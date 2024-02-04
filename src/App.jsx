aa5c457a0872c0604302d0dc27f9d47fef80fdd9
KarimRezieka <karimsaadrezieka@outlook.com>
2024-02-04 18:28:29 +0200

    Navbar&hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./app.scss";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return <div>
+    <section id="HomePage"><Navbar/>
+    <Hero/>
+    </section>
     <section>Hero</section>
     <section>Parallax</section>
     <section>Services</section>
@@ -8,7 +13,7 @@ const App = () => {
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio2</section>
-    <section>Contact</section>
+    <section id="Contact">Contact</section>
   </div>;
 };
 

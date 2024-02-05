77d1f364d56bad5c09e52183c5e66f5b15844a7d
KarimRezieka <karimsaadrezieka@outlook.com>
2024-02-05 23:31:42 +0200

    Send Email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,73 +1,81 @@
-import { useRef } from 'react';
-import './contact.scss'
-import {motion} from 'framer-motion'
-import emailjs from '@emailjs/browser'
-const variants={
-    initial:{
-        y:500,
-        opacity:0
+import { useRef, useState } from "react";
+import "./contact.scss";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+const variants = {
+  initial: {
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.1,
     },
-    animate:{
-        y:0,
-        opacity:1,
-        transition:{
-            duration:0.5,
-            staggerChildren:0.1,
-        },
-    },
+  },
 };
-const ServiceId = process.env.YOUR_SERVICE_ID
-const TemplateID = process.env.YOUR_TEMPLATE_ID
-const Publickey = process.env.YOUR_PUBLIC_KEY
+const serviceID = process.env.YOUR_SERVICE_ID
+const templateID = process.env.YOUR_TEMPLATE_ID
+const key = process.env.YOUR_PUBLIC_KEY
+function Contact() {
+    // eslint-disable-next-line no-undef
 
-console.log(process.env.YOUR_PUBLIC_KEY)
-function Contact(){
-    const formRef = useRef()
-    const sendEmail = (e) => {
-        e.preventDefault();
-    
-        emailjs
-          .sendForm('', '', form.current, {
-            publicKey: '',
-          })
-          .then(
-            () => {
-              console.log('SUCCESS!');
-            },
-            (error) => {
-              console.log('FAILED...', error.text);
-            },
-          );
-      };
+  const formRef = useRef();
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("serviceID", "templateID", formRef.current, {
+        publicKey: "key",
+      })
+      .then(
+        (result) => {
+          setSuccess(true);
+        },
+        (error) => {
+          setError(true);
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
 
-    return(
-        <motion.div className="contact" variants={variants} initial="initial" whileInView="animate">
-            <motion.div className="textContainer" variants={variants}>
-                <motion.h1 variants={variants}>Let`s work to gether</motion.h1>
-                <motion.div className="item" variants={variants}>
-                    <h4>Mail</h4>
-                    <p>hello@react.dev</p>
-                </motion.div>
-                <motion.div className="item" variants={variants}>
-                    <h4>Address</h4>
-                    <p>123 New York</p>
-                </motion.div>
-                <motion.div className="item" variants={variants}>
-                    <h4>Phone</h4>
-                    <p>+1 258 5687</p>
-                </motion.div>
-            </motion.div>
-            <div className="formContainer">
-
-                <form action="" ref={formRef}>
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email" />
-                    <textarea rows={8} placeholder="Message" ></textarea>
-                    <button>Submit</button>
-                </form>
-            </div>
+  return (
+    <motion.div
+      className="contact"
+      variants={variants}
+      initial="initial"
+      whileInView="animate"
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <motion.h1 variants={variants}>Let`s work to gether</motion.h1>
+        <motion.div className="item" variants={variants}>
+          <h4>Mail</h4>
+          <p>hello@react.dev</p>
         </motion.div>
-    )
+        <motion.div className="item" variants={variants}>
+          <h4>Address</h4>
+          <p>123 New York</p>
+        </motion.div>
+        <motion.div className="item" variants={variants}>
+          <h4>Phone</h4>
+          <p>+1 258 5687</p>
+        </motion.div>
+      </motion.div>
+      <div className="formContainer">
+        <form  ref={formRef}  onSubmit={sendEmail}>
+          <input type="text" placeholder="Name" name="name"/>
+          <input type="email" placeholder="Email" name="email"/>
+          <textarea rows={8} placeholder="Message" name="message"></textarea>
+          <button>Submit</button>
+          {error && "Error"}
+          {success && "Success"}
+        </form>
+      </div>
+    </motion.div>
+  );
 }
 
-export default Contact ;
+export default Contact;
